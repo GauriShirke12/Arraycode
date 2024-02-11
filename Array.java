31a6f39914a882a1e56cd49df5ae94acccf7e99a
GauriShirke12 <152776730+GauriShirke12@users.noreply.github.com>
2024-02-11 11:12:21 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Array {
+   public static void main(String[] args) {
+    int a[] = new int[50];
+
+Scanner sc =new Scanner(System.in);
+System.out.println("enter physics marks");
+int phy;
+phy=sc.nextInt();
+System.out.println("marks is:"+phy);
+   }
+}
